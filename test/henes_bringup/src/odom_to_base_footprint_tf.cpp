d667e0ab3a8e2e476d6265ea7985a070d80a53f2
kimjunho654 <105560901+kimjunho654@users.noreply.github.com>
2023-05-06 14:56:50 +0900

    Add files via upload

--- test/henes_bringup/src/odom_to_base_footprint_tf.cpp
+++ test/henes_bringup/src/odom_to_base_footprint_tf.cpp
@@ -1,14 +1,14 @@
 #include <ros/ros.h>
-#include <tf2_msgs/TFMessage.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "odom_base_footprint_tf_publisher");
     ros::NodeHandle nh;
 
-    ros::Publisher tf_pub = nh.advertise<tf2_msgs::TFMessage>("/tf", 1);
 
-    tf2_msgs::TFMessage tf_msg;
+
+    tf2_ros::TransformBroadcaster tf_msg;
     geometry_msgs::TransformStamped transformStamped;
 
     ros::Rate loop_rate(10);
@@ -23,9 +23,8 @@ int main(int argc, char** argv){
         transformStamped.transform.rotation.y = 0.0;
         transformStamped.transform.rotation.z = 0.0;
         transformStamped.transform.rotation.w = 1.0;
-        tf_msg.transforms.push_back(transformStamped);
-        tf_pub.publish(tf_msg);
-        tf_msg.transforms.clear();
+        tf_msg.sendTransform(transformStamped);
+
         loop_rate.sleep();
     }
 
